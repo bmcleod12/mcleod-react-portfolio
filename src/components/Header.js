0a2d492a4be165628997934abc09cfe842414595
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-05-09 20:03:52 -0500

    formatted and changed class to className

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,43 +6,43 @@ function Header() {
   const location = useLocation();
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
         <Link to="/" className="navbar-brand">Brenna McLeod</Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-          <li className="nav-item">
-       <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-      Home
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                Home
          </Link>
-       </li>
-     <li className="nav-item">
-         <Link
-              to="/about"
-              className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
-            >
-              About
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/about"
+                className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+              >
+                About
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/portfolio"
-              className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-            >
-              Portfolio
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/portfolio"
+                className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+              >
+                Portfolio
             </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/contact"
-              className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-            >
-              Contact
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+              >
+                Contact
             </Link>
-          </li>
+            </li>
           </ul>
         </div>
       </div>
