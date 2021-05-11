480bb542a2626d389c85c6321cc1eb06341e5c7f
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-05-10 20:59:05 -0500

    updates to home page hero image to shrink the image down to size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,18 +3,24 @@ import backgroundImage from "../images/kc-shuffle.jpg"
 
 function Hero() {
     return (
-        <div>
-        <img src={backgroundImage} alt="We're not in Kansas anymore."></img>
-          <div className="col-md-5 p-5 offset-md-7">
-            <div className="card bg-gray-600 bg-opacity-60 text-gray-50">
-              <div className="card-body">
-                <h5 className="card-title text-3xl">Hi, I'm Brenna.</h5>
-                <p className="card-text text-5xl py-3">Kansas City-based full stack developer excited to work with you.</p>
-                {/* <a href="" className="btn bg-green-800 text-gray-50 my-3">Learn More ></a> */}
-              </div>
+        <div className="pt-5"
+         style={{
+            backgroundImage: `url(${backgroundImage})`, 
+            backgroundSize: "cover",
+            backgroundPosition: "top right",
+            backgroundRepeat: "no-repeat",
+            height: "60rem"
+        }}>
+            <div className="col-md-5 p-5 offset-md-7">
+                <div className="card bg-dark text-light">
+                    <div className="card-body bg-gray text-gray-50">
+                        <h3 className="card-title">Hi, I'm Brenna.</h3>
+                        <h1 className="card-text text-5xl py-3">Kansas City-based full stack developer excited to work with you.</h1>
+                        {/* <a href="" className="btn bg-green-800 text-gray-50 my-3">Learn More ></a> */}
+                    </div>
+                </div>
             </div>
-          </div>
-      </div>
+        </div>
     );
 }
 
